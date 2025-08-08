48c688f89a6020d683f8be84db86a8ba91534c49
Coen Bakker <106626890+coenbakker@users.noreply.github.com>
2025-08-08 15:56:24 +0200

    Create empty mix project

---
+++ lib/blink.ex
@@ -0,0 +1,18 @@
+defmodule Blink do
+  @moduledoc """
+  Documentation for `Blink`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Blink.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
